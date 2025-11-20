3dd307b7fb0944eef8b503b5facdb411a9d0259a
nityasavadatti <nityasavadatti39@gmail.com>
2025-11-20 14:06:37 +0530

    Create sys.py

---
+++ sys.py
@@ -0,0 +1,13 @@
+import sys
+#check if correct number of arguments a
+if len(sys.argv)!=3:
+  print("usage:python student.py<name><roll>")
+        sys.exit(1)
+      #sys.argv[0] is always the program name
+        script_name=sys.argv[0]
+  name=sys.argv[1]
+  rollno=sys.argv[2]
+
+print("script name:",script_name)
+print("student name:",name)
+print("roll numnber:",rollno)
