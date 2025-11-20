f3a08f5740f96123701aa6c573294f0b72dad446
nityasavadatti <nityasavadatti39@gmail.com>
2025-11-20 14:43:32 +0530

    Update sys.py

--- sys.py
+++ sys.py
@@ -4,9 +4,9 @@ if len(sys.argv)!=3:
   print("usage:python sys.py<name><roll>")
   sys.exit(1)
 #sys.argv[0] is always the program name
-  script_name=sys.argv[0]
-  name=sys.argv[1]
-  rollno=sys.argv[2]
+script_name=sys.argv[0]
+name=sys.argv[1]
+rollno=sys.argv[2]
 
 print("script name:",script_name)
 print("student name:",name)
