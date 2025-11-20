97b5c6dfb3fa64c1bef4f00c15ba28ca8df7025b
nityasavadatti <nityasavadatti39@gmail.com>
2025-11-20 14:39:24 +0530

    Update sys.py

--- sys.py
+++ sys.py
@@ -2,9 +2,9 @@ import sys
 #check if correct number of arguments a
 if len(sys.argv)!=3:
   print("usage:python sys.py<name><roll>")
-        sys.exit(1)
-      #sys.argv[0] is always the program name
-        script_name=sys.argv[0]
+  sys.exit(1)
+#sys.argv[0] is always the program name
+  script_name=sys.argv[0]
   name=sys.argv[1]
   rollno=sys.argv[2]
 
